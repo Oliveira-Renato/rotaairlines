7dd195a72c4bf364136dcb3db9754b0dda533f36
Renato Oliveira <renato.printf@gmail.com>
2024-01-17 16:13:24 -0300

    user model

--- view/cadastro.php
+++ view/cadastro.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="view/styles/style.css">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
@@ -165,7 +165,7 @@
   </div>
 
   <!-- Footer-->
-  <?php include("footer.php") ?>
+  <?php include("view/footer.php") ?>
 </body>
 
 </html>
